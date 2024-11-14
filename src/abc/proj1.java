96db0fee7c3cd664067ed40de76b70808a761cdf
AUTBLT210 <AUTBLT210@LT210.allianceblr.com>
2024-11-14 15:10:41 +0530

    commit

---
+++ src/abc/proj1.java
@@ -0,0 +1,12 @@
+package abc;
+
+public class proj1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Alliance");
+		System.out.println("Welcome to CSE General B");
+
+	}
+
+}
